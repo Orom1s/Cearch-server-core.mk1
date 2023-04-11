8c6f21cba40e51a5a6b7f468aa2f201fc6455204
Orom1s <zenkincw@gmail.com>
2023-04-11 16:55:06 +0300

    up

--- Server/search_server.h
+++ Server/search_server.h
@@ -160,3 +160,9 @@ std::vector<Document> SearchServer::FindAllDocuments(const Query& query,const Ke
         }
         return matched_documents;
     }
+
+void AddDocument(SearchServer& search_server, int document_id, const std::string& document, DocumentStatus status, const std::vector<int>& ratings);
+
+void FindTopDocuments(const SearchServer& search_server, const std::string& raw_query);
+
+void MatchDocuments(const SearchServer& search_server, const std::string& query);
